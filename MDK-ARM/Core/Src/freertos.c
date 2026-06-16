3626269b4bb497c212addf5d638c105c010a941c
ajkajkbkc <pho968@qq.com>
2026-06-16 16:31:50 +0800

    添加串口任务

--- MDK-ARM/Core/Src/freertos.c
+++ MDK-ARM/Core/Src/freertos.c
@@ -54,6 +54,33 @@ const osThreadAttr_t defaultTask_attributes = {
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityNormal,
 };
+/* Definitions for UartTask */
+osThreadId_t UartTaskHandle;
+const osThreadAttr_t UartTask_attributes = {
+  .name = "UartTask",
+  .stack_size = 512 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for UartQueue */
+osMessageQueueId_t UartQueueHandle;
+const osMessageQueueAttr_t UartQueue_attributes = {
+  .name = "UartQueue"
+};
+/* Definitions for UartTimer1 */
+osTimerId_t UartTimer1Handle;
+const osTimerAttr_t UartTimer1_attributes = {
+  .name = "UartTimer1"
+};
+/* Definitions for UartTimer2 */
+osTimerId_t UartTimer2Handle;
+const osTimerAttr_t UartTimer2_attributes = {
+  .name = "UartTimer2"
+};
+/* Definitions for UartTimer3 */
+osTimerId_t UartTimer3Handle;
+const osTimerAttr_t UartTimer3_attributes = {
+  .name = "UartTimer3"
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -61,6 +88,8 @@ const osThreadAttr_t defaultTask_attributes = {
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
+void vUartTask(void *argument);
+void UartTimerCallback(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -82,9 +111,23 @@ void MX_FREERTOS_Init(void) {
   /* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
 
+  /* Create the timer(s) */
+  /* creation of UartTimer1 */
+  UartTimer1Handle = osTimerNew(UartTimerCallback, osTimerPeriodic, (void*) 1, &UartTimer1_attributes);
+
+  /* creation of UartTimer2 */
+  UartTimer2Handle = osTimerNew(UartTimerCallback, osTimerPeriodic, (void*) 2, &UartTimer2_attributes);
+
+  /* creation of UartTimer3 */
+  UartTimer3Handle = osTimerNew(UartTimerCallback, osTimerPeriodic, (void*) 3, &UartTimer3_attributes);
+
   /* USER CODE BEGIN RTOS_TIMERS */
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of UartQueue */
+  UartQueueHandle = osMessageQueueNew (10, sizeof(uint16_t), &UartQueue_attributes);
 
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
@@ -93,6 +136,9 @@ void MX_FREERTOS_Init(void) {
   /* Create the thread(s) */
   /* creation of defaultTask */
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of UartTask */
+  UartTaskHandle = osThreadNew(vUartTask, NULL, &UartTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -118,11 +164,37 @@ void StartDefaultTask(void *argument)
   for(;;)
   {
 		HAL_GPIO_TogglePin(LED_G_GPIO_Port,LED_G_Pin);
-		osDelay(100);
+		osDelay(1000);
 		HAL_GPIO_TogglePin(LED_R_GPIO_Port,LED_R_Pin);
-		osDelay(100);
+		osDelay(1000);
   }
   /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_vUartTask */
+/**
+* @brief Function implementing the UartTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_vUartTask */
+void vUartTask(void *argument)
+{
+  /* USER CODE BEGIN vUartTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END vUartTask */
+}
+
+/* UartTimerCallback function */
+void UartTimerCallback(void *argument)
+{
+  /* USER CODE BEGIN UartTimerCallback */
+
+  /* USER CODE END UartTimerCallback */
 }
 
 /* Private application code --------------------------------------------------*/
